2926bcd711b49caabc3b37b87b0216bdcf032ec8
mbarto202 <michael.d.barto@gmail.com>
2024-08-16 09:59:03 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+// console.log('Hello')
+
+// let age = 25
+// console.log(age)
+
+// const salary = 80000
+// console.log(salary)
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+// const name = "Vishwas";
+// const language = "JavaScript";
+// const channel = `Codevolution`;
+
+// const total = 0;
+// const PI = 3.14;
+
+// const isPrimaryNumber = true;
+// const isNewUser = false;
+
+// let result;
+// console.log(result)
+
+// const res = undefined; // not yet defined
+
+// const data = null; // null value
+
+//----------Data Types------------------
+
+// const person = {
+//   firstName: "Bruce",
+//   lastName: "Wayne",
+//   age: 30,
+// };
+
+// console.log(person.firstName);
+
+// const oddNumbers = [1, 3, 5, 7, 9];
+// console.log(oddNumbers[2])
+
+// let a = 10
+// a = 'Vishwas'
+// a = true
+// console.log(a)
+
+//------------Operators----------------
+
+// let x = 10
+// let y = 5
+// console.log(x % y)
+// console.log(++x)
+// console.log(--y)
+
+// console.log(x >= y)
+
+// const isValidNumber = x > 8 && 8 > y
+// console.log(isValidNumber)
+
+// const isValid = false
+// console.log(!isValid);
+
+// console.log('Bruce ' + 'Wayne');
+
+// const isEven = 10 % 2 === 0 ? 'number is even' : 'number is odd'
+// console.log(isEven);
+
+//--------Type Conversions------------
+
+// console.log(true + '3');
+// console.log('8' / '2');
+// console.log('bruce' - 'wayne');
+// console.log('5' - null);
+// console.log(5 - undefined);
+
+// console.log(parseFloat('3.14'));
+// console.log(String(500));
+// console.log((500).toString()); // will not work on null or undefined
+// console.log(Boolean(10)); // null undefined 0 ' ' NaN
+
+//----------Equality---------------
+const var1 = 'test'
+const var2 = 'test'
+
+console.log(var1 == var2);
+console.log(var1 === var2);
+
