f9ad6efacd0010f3a7525441f21d4a9ca7ea2a15
mbarto202 <michael.d.barto@gmail.com>
2024-08-16 10:43:28 -0400

    completed

--- main.js
+++ main.js
@@ -80,9 +80,96 @@
 // console.log(Boolean(10)); // null undefined 0 ' ' NaN
 
 //----------Equality---------------
-const var1 = 'test'
-const var2 = 'test'
 
-console.log(var1 == var2);
-console.log(var1 === var2);
+// const var1 = '10'
+// const var2 = 10
 
+// console.log(var1 == var2); // automatically converts then compares
+// console.log(var1 === var2); // must be same type
+
+//-----Conditional Statements------
+
+// const num = 0;
+
+// if (num > 0) {
+//   console.log("number is positive");
+// } else if (num < 0) {
+//   console.log("number is not positive");
+// } else {
+//   console.log("number is zero");
+// }
+
+// const color = "red";
+
+// switch (color) {
+//   case "red":
+//     console.log("color is red");
+//     break;
+//   case "blue":
+//     console.log("color is blue");
+//     break;
+//   default:
+//     console.log("not valid color");
+// }
+
+//----------Looping Code-------------
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log("iteration number " + i);
+// }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log("iteration number " + i);
+//   i++;
+// }
+
+// let i = 6;
+// do {
+//   console.log("iteration number " + i);
+//   i++;
+// } while (i <= 5);
+
+// const numArray = [1, 2, 3, 4, 5];
+
+// for (const num of numArray) {
+//   console.log("iteration number " + num);
+// }
+
+//----------Functions---------------
+
+// function greet(username) {
+//   console.log("good morning " + username);
+// }
+
+// greet("Bruce");
+// greet("Wayne");
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const arrowSum = (a, b) => a + b;
+// const addFive = num => num + 5;
+
+// const sum = arrowSum(25, 25);
+// console.log(sum);
+
+//-----------Scope---------------
+
+// const myNum = 100;
+// const myName = "superman";
+
+// if (true) {
+//   const myName = "michael";
+//   console.log(myName);
+//   console.log(myNum);
+// }
+
+// function testFn() {
+//   const myName = "batman";
+//   console.log(myName);
+//   console.log(myNum);
+// }
+
+// testFn();
